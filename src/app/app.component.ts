0cd22e26a6a350090d9e7a2e1bd82b084f47e9a1
Anandhu KS <anandhu.ks@bankopen.co>
2025-02-12 18:17:36 +0530

    angular update to 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [CommonModule, RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    standalone:true
 })
 export class AppComponent {
   title = 'ai-app';
--- src/components/chat-interface/chat-interface.component.ts
+++ src/components/chat-interface/chat-interface.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { SideNavComponent } from '../side-nav/side-nav.component';
 
 @Component({
-  selector: 'app-chat-interface',
-  standalone: true,
-  imports: [SideNavComponent],
-  templateUrl: './chat-interface.component.html',
-  styleUrl: './chat-interface.component.css',
+    selector: 'app-chat-interface',
+    imports: [SideNavComponent],
+    standalone:true,
+    templateUrl: './chat-interface.component.html',
+    styleUrl: './chat-interface.component.css'
 })
 export class ChatInterfaceComponent {}
