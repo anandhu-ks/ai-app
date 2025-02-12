26de74b15e5ccfa701f806b89bacab6e302ef8c8
Anandhu KS <anandhu.ks@bankopen.co>
2025-02-12 18:09:33 +0530

    add sidenav

--- src/components/chat-interface/chat-interface.component.ts
+++ src/components/chat-interface/chat-interface.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { SideNavComponent } from '../side-nav/side-nav.component';
 
 @Component({
   selector: 'app-chat-interface',
   standalone: true,
-  imports: [],
+  imports: [SideNavComponent],
   templateUrl: './chat-interface.component.html',
-  styleUrl: './chat-interface.component.css'
+  styleUrl: './chat-interface.component.css',
 })
-export class ChatInterfaceComponent {
-
-}
+export class ChatInterfaceComponent {}
